5c6acc757344da153b30c585619e37f10363bb04
Maxim Kolchin <kolchinmax@gmail.com>
2011-04-13 21:56:23 +0400

    Changed style of main page

--- app/media/js/base.js
+++ app/media/js/base.js
@@ -4,3 +4,20 @@
  * Time: 22:46:38
  */
 
+$(document).ready(function() {
+    $('#coin-slider').coinslider(
+        {
+        width: 850,
+        height: 300,
+        spw: 3,
+        sph: 5,
+        navigation: true,
+        delay: 5000,
+        links: false,
+        effect: 'rain',
+        titleSpeed: 500,
+        sDelay: 50,
+        opacity: 0.4
+        });
+});
+
