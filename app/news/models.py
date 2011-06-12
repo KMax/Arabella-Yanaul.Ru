0c6ce34d8f7b5dd9f90cdc82c60bccf0dc77e030
Maxim Kolchin <kolchinmax@gmail.com>
2011-06-12 14:43:19 +0400

    Slide show was added on start page. And the design was changed.

--- app/news/models.py
+++ app/news/models.py
@@ -7,6 +7,19 @@ class News(models.Model):
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     text = models.TextField(verbose_name="Текст новости")
 
+    def intro(self):
+        text = unicode(News.objects.get(id=self.id).text).\
+        partition("<!--pagebreak-->")
+        return text[0]
+
+    def is_short_news(self):
+        text = unicode(News.objects.get(id=self.id).text).\
+        partition("<!--pagebreak-->")
+        if text[1]: 
+            return False
+        else:
+            return True
+
     class Meta:
         db_table = 'news'
         verbose_name = 'новость'
--- app/news/views.py
+++ app/news/views.py
@@ -2,6 +2,21 @@
 from django.shortcuts import render_to_response
 from news.models import News
 
+news_per_page = 4
+
 def start_page(request):
-    news_list = News.objects.all()
-    return render_to_response('news.html', locals())
+    return get_page(1)
+
+def get_news(request, id='1'):
+    news = News.objects.get(id=id)
+    return render_to_response('news.html',locals())
+
+def get_page(request, page_id='1'):
+    page_id = int(page_id)
+    if page_id > 1:
+        pred_page_id = page_id - 1
+    if News.objects.count() > news_per_page*page_id:
+        next_page_id = page_id + 1
+    news_list = News.objects.\
+                order_by('-date')[news_per_page*(page_id-1):news_per_page*page_id]
+    return render_to_response('news_list.html', locals())
